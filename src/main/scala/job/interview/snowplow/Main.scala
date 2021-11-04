31f13f5db391f4632c221b2d55ca50c9fc68ea20
Przemek Dyk <przemek.dyk@gmail.com>
2021-11-04 15:43:48 +0100

    empty project from http4s/http4s.g8 template

---
+++ src/main/scala/job/interview/snowplow/Main.scala
@@ -0,0 +1,8 @@
+package job.interview.snowplow
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    JsonvalidationserviceServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
